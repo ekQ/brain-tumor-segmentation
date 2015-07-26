9a360025bc8d04fcd019360dce2bf02fbb72619d
shameem <shameem.ahamed@hotmail.com>
2015-07-26 13:46:28 +0300

    with iris data

--- sparse_encode.py
+++ sparse_encode.py
@@ -1,16 +1,20 @@
+from sklearn import datasets;
 from skimage import data;
 import spams;
 import numpy as np;
 from PIL import Image
 from pylab import *
 
-X1 = np.random.randint(255,size=(128,128));
-X2 = np.random.randint(255,size=(128,128));
-X3 = np.random.randint(255,size=(128,128));
+iris = datasets.load_iris();
 
-X = np.hstack((X1,X2,X1,X3,X2,X3));
-imshow(X,cmap="Greys_r");
-show();
+X=iris.data;
+#X1 = np.random.randint(255,size=(128,128));
+#X2 = np.random.randint(255,size=(128,128));
+#X3 = np.random.randint(255,size=(128,128));
+
+#X = np.hstack((X1,X2,X1,X3,X2,X3));
+#imshow(X,cmap="Greys_r");
+#show();
 #X = data.camera();
 #X = X/255. ;
 mx = np.max(np.max(X,axis=0));
@@ -23,24 +27,25 @@ else:
 	A = np.asfortranarray(X);
 	rgb = False;
 #print A.shape;
-m = 128;	#No of rows in the image
-n = 128;	#No of cols in the image
-#A new matrix with 512*512 rows is created. One image corresponds to one column. In fact we do sliding img2col, so if we want we can split the image into small blacks.
+m = 150;	#No of rows in the image (no of images)
+n = 1;	#No of cols in the image (no of features)
+#A new matrix with m*n rows is created. One image corresponds to one column. In fact we do sliding img2col, so if we want we can split the image into small blacks.
 X = spams.im2col_sliding(A,m,n,rgb);
 X = X - np.tile(np.mean(X,0),(X.shape[0],1));
 X = np.asfortranarray(X / np.tile(np.sqrt((X * X).sum(axis=0)),(X.shape[0],1)),dtype = float);
 
-#print X.shape;
+print X.shape;
 K = 3;
-param = { 'K' : K, 'mode' : 0, 'modeD' : 0, 'lambda1' : 0.15, 'numThreads' : 4, 'batchsize' : 400, 'iter' : 50};
+param = { 'K' : K, 'mode' : 0, 'modeD' : 0, 'lambda1' : 0.15, 'numThreads' : 4, 'batchsize' : 400, 'iter' : 100};
 
 D = spams.trainDL(X,**param);
-#print D.shape;
+print D.shape;
 O=np.zeros((m,n*K),dtype=float);
 for i in range(D.shape[1]):
 	O[0:m,i*n:i*n+n] = reshape(D[:,i],(m,n),order='F');
 
+print O.shape
 #print mx*D;
-imshow(O,cmap="Greys_r");
-show();
+#imshow(O,cmap="Greys_r");
+#show();
 #X = data.camera();
